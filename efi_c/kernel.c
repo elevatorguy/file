7a9e5c3139a8fadceeb626f65166953976247b49
Jason Garner <elevatorguy@gmail.com>
2025-10-06 14:58:10 -0500

    only zero

--- efi_c/kernel.c
+++ efi_c/kernel.c
@@ -67,18 +67,23 @@ noreturn void EFIAPI kmain(Kernel_Parms *kargs) {
         text2[i] = '\0';
     }
 
-    bs->CreateEvent(EVT_TIMER | EVT_NOTIFY_SIGNAL,
-                        TPL_CALLBACK,
-                        update_text,
-                        (VOID *)kargs,
-                        &timer_event);
-
-    bs->SetTimer(timer_event, TimerPeriodic, 1000000);
-
     // Print test string(s)
     x = y = 0;  // Reset to 0,0 position
     Bitmap_Font *font1 = &kargs->fonts[0];
     Bitmap_Font *font2 = &kargs->fonts[1];
+    print_string("0\r\n", font2);
+
+    bs->CreateEvent(EVT_TIMER | EVT_NOTIFY_SIGNAL,
+                        TPL_CALLBACK,
+                        update_text,
+                        (VOID *)kargs->RuntimeServices,
+                        &timer_event);
+
+    print_string("1\r\n", font2);
+
+    bs->SetTimer(timer_event, TimerPeriodic, 1000000);
+
+    print_string("2\r\n", font2);
 
     while (true) {
         EFI_KEY_DATA data = {0};
