c5ad6aa5ec0c7c9dab55890a122ff92e200ac573
Jason Garner <elevatorguy@gmail.com>
2026-05-18 16:12:36 -0500

    file

---
+++ efi_c/file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+DISK=""
+DISK1="/dev/null"
+DISK2="/dev/null"
+DISK3="/dev/null"
+
+sudo losetup /dev/loop30 /tmp/file.img
+sudo kpartx -a /dev/loop30
+#lsblk
+sudo mount /dev/mapper/loop30p1 /media/flash/one
+sudo mount UUID="$DISK" /media/flash/two
+sudo cp /media/flash/one/EFI/BOOT/FILE.TXT /media/flash/two/EFI/BOOT/
+sudo cp /media/flash/one/EFI/BOOT/FILE.EFI /media/flash/two/EFI/BOOT/BOOTX64.EFI
+sudo umount /media/flash/one
+sudo umount /media/flash/two
+sudo dd if=/dev/mapper/loop30p2 of="$DISK1"
+sudo dd if=/dev/mapper/loop30p2 of="$DISK2"
+sudo dd if=/dev/mapper/loop30p2 of="$DISK3"
+sudo kpartx -d /dev/loop30
+sudo losetup -d /dev/loop30
